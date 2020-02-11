f5921edaa11c8a9a738daeec419d5c6a6a9baeb0
Schoolbox_95 <bpudsey@qmail.qcc.edu>
2020-02-10 22:21:09 -0500

    initial scaffold

---
+++ src/App.svelte
@@ -0,0 +1,170 @@
+<script>
+	let site_header = "~ l u r l i n a ~";
+
+</script>
+<style>
+div::-webkit-scrollbar {
+  display: none;
+}
+div.service {
+	border-style:dotted;
+	border-width: 1px;
+}
+div {
+	font-family: "Courier New";
+}
+
+</style>
+<div id='site_header' style="display: flex;justify-content: center;align-items: center;height:20%;width:100%;border-style:solid;">
+{site_header}
+</div> 
+<div id='site_body' style="height:80%;width:100%;border-style:solid;overflow-y:scroll;overflow-x:hidden;">
+	<div id='logo_space' >
+		<p align:left style="margin-top:0px;text-align:center; font-weight:bold">
+			Housecall Hair Stylist<br>Punk,<br>Cosplay,<br>Fantasy,<br>&<br> 80's Retro <br>Specialist
+		</p>
+	</div>	
+	<div id='articles'>
+		<div style="padding:10px;position:relative;background-image: linear-gradient(to right, #FFb5e8, #b28dff, #dcd3ff, #aff8d8, #bffcc6, #ffc9de );">
+			<h4 style="margin:0px;"> [Fantasy Hair Specialities]	</h4><div style="display:flex;overflow-x:scroll;">
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+			</div>	
+		</div>
+		<div style="padding:10px; background-image:linear-gradient(to left, #453F46, #CF3E61, #E68A5D, #C9C79A, #56999D);">
+			<h4 style="margin:0px;"> [Punk Hair Specialties]	</h4><div style="display:flex;overflow-x:scroll;">
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+			</div>	
+		</div>
+		<div style="padding:10px; background-image:linear-gradient(to right, #ffd4b2, #f4eaeb, #fdd4e4, #996c69, #65524b);">
+			<h4 style="margin:0px;"> [80s Hair Specialties]	</h4>
+			<div style="display:flex;overflow-x:scroll;">
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+			</div>	
+		</div>
+		<div style="padding:10px">
+			<h4 style="margin:0px;"> [Wig Dyes and Styling]	</h4><div style="display:flex;overflow-x:scroll;">
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+				<div style="border:dotted;border-width:1px;min-height:50px;min-width:50px;display:flex;justify-content:center;align-items:center;">^__^</div>
+			</div>	
+		</div>
+	</div>
+	<div id='reviews'></div>
+	<div id='services'>
+		<div class="service">
+			<h5 style="margin:0x;">Women's Hair Cuts		</h5>
+			<p>A good haircut creates confidence, ease, and style.  It can be life changing.    It is essential that your haircut compliment your facial features, your hair’s individual texture and thickness, and head shape.  Your stylist will also discuss styling techniques along with product recommendations to maintain your style.  Service includes shampoo and blow out! 	<br> starting at $45		</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Blow Outs			</h5>
+			<p>It takes practice and skill to create a beautiful hairstyle; but, once mastered, it can last for days.  Whether you want a break from styling your own hair or a special event requires a more polished look, this service is for you.  			<br> starting at $30			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Special Occasion Hairstyles / Upstyles			</h5>
+			<p>Don’t let a special occasion be a disappointment due to ordinary hair.  We can create glamorous curls to a classic chignon for weddings, galas, or a hot date…the possibilities are endless.  			<br> starting at $50			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Men's Hair Cuts			</h5>
+			<p>Men’s cuts are more versatile now than ever before.  Whether a tight fade, a classic business cut, or the latest trend, our stylists can create a cut suitable for your lifestyle, occupation, or preference. 			<br> starting at $25			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Beard Grooming			</h5>
+			<p>For those men whose hair on their head just isn’t enough…Let us help design a unique look to compliment your features. 			<br> starting at $15			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Bang Trimmings			</h5>
+			<p>One complimentary “clean up” bang trim between haircuts			<br> starting at $15			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Children's Hair Cuts			</h5>
+			<p>Make it a family affair! Our stylists are always happy to take care of your littles (12 & under).			<br> starting at $25			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Ombre Hair & Balayage			</h5>
+			<p>Ombre? Sombre? Color Melt? Balayage?  Are you intrigued by these modern highlighting looks but not really sure what they mean or how they would work for you?   We will consult with you and customize a unique look just for you! 			<br> starting at $165			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Hair Color Correction			</h5>
+			<p>Hair dye is too dark or the wrong color altogether?  Removing box hair dye and restoring your hair to a flattering color is a process.  We will explain the process and discuss realistic expectations of this service. 			<br> starting at $165			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Natural Blondie		</h5>
+			<p>This “blonding” color technique gives the most natural looking blonde hair results. Softening your root color while keeping ends lighter will extend your maintenance between appointments. Includes a partial foil, base break, glaze and blow out. 			<br> starting at $175			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Bombshell Blonding			</h5>
+			<p>Suited for the client that wants a full head of blonde hair.  This can be accomplished with a full head of foils or a global (all over) lightener.  This process includes removing unwanted tones if necessary and a new re-toning			<br> starting at $165			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Take Me Down a Notch			</h5>
+			<p>Have you gone too far with the bleach?  Do your blonde highlights make you a little too blonde or as we call it “too solid?”  We can add ammonia-free color to re-pigment selected strands of your hair and give you back the natural looking color and dimension you desire! 			<br> starting at $100			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Hair Painting or Balayage	</h5>
+			<p>Hair painting is a freestyle way of highlighting hair. It is a specialized and customized service.  The color or lightener is painted directly on to the hair, creating a gradient of color, shadows and depth. Only certain stylists are trained in this advanced balayage technique.  During the process, a glaze or gloss is applied.			<br> starting at $145			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Fantasy Hair Color			</h5>
+			<p>Vibrant hair colors, also known as Fantasy Colors, are a journey.  In order to attain and maintain these shades, the hair must be properly lightened and conditioned. A consultation on desired color or colors must be made prior to appointment.			<br> starting at $200			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Single-Process Root Touch Up		</h5>
+			<p>This service covers new growth of your roots.  An appropriate type of color will be formulated for best coverage.  Services varies dependent on individual needs.			<br> starting at $60			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Double-Process Color with Foils			</h5>
+			<p>A base color is applied and foil highlights are placed for dimension. 			<br> starting at $125			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Partial Head Foil Highlights			</h5>
+			<p>The crown of the head is foiled with one color.  There is a $10 charge for any extra colors added.  Blow out is included.			<br> starting at $75			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">full Head Foil Highlights			</h5>
+			<p>Traditional foils are applied to the entire head.  There is a $20 extra charge for each additional color.  Blow out is included. 			<br> starting at $125			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Double Proccess Lightner		</h5>
+			<p>This will give you an all over blonde look.  First we lighten the entire head and then tone with a glaze.  Includes a blow out. 			<br> starting at $100			</p>
+		</div>
+		<div class="service">
+			<h5 style="margin:0x;">Men's Camo Color			</h5>
+			<p>Customized grey camouflage formulated just for men!  For the man who doesn’t want his hair color to be obvious, but rather blend naturally.			<br> starting at $35			</p>
+		</div>
+	</div>
+</div>
+
